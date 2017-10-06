d6d643d8ce533926785514c8f039b18796ea08d2
Arvind D <arvindd25@gmail.com>
2017-10-05 17:09:07 -0700

    added the format for fixtures

--- testEx.py
+++ testEx.py
@@ -1,15 +1,29 @@
 import sys
 import nose
 import unittest
+from nose import with_setup
 from Plugins.htmlReport import HtmlOutput
 
 class MyTest(unittest.TestCase):
+    def my_setup_function():
+        pass
+
+    def my_teardown_function():
+        pass
+
+    @with_setup(my_setup_function, my_teardown_function)
     def test_a_test(self):
         assert False
+
+    @with_setup(my_setup_function, my_teardown_function)
     def test_b_test(self):
         assert True
+
+    @with_setup(my_setup_function, my_teardown_function)
     def test_c_test(self):
         assert False
+
+    @with_setup(my_setup_function, my_teardown_function)
     def test_d_test(self):
         assert True
 
